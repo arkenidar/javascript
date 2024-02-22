f514ed4840a7f28964aae1ec5dca14160d012eaa
Dario Cangialosi a.k.a. Arkenidar <dario.cangialosi@gmail.com>
2024-02-22 12:06:10 +0100

    better logger() function with multiple parameters also, non-string parameters also

--- nascita.js
+++ nascita.js
@@ -2,6 +2,7 @@
 if(typeof logger=="undefined")
     var logger = function(...args){ console.log(...args) }
 
+logger("Logger", "version 1.0")
 function FormatDateItalianToInternational(data) { // "GG/MM/AAAA" => "AAAA-MM-GG"
     return data.split("/").reverse().join("-")
 }
@@ -11,4 +12,4 @@ var date = new Date(FormatDateItalianToInternational("22/02/2024"))
 var pad = number => String(number).padStart(2, '0')
 var format = date => `${date.getFullYear()}/${pad(date.getMonth() + 1)}/${pad(date.getDate())}`
 logger(format(date))
-logger(date)
+logger(date.toString())
