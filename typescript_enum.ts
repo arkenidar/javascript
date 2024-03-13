243a4001537b301c51976ecd946ff426246752c2
Dario Cangialosi a.k.a. Arkenidar <dario.cangialosi@gmail.com>
2024-03-13 21:32:17 +0100

    typescriptlang.org/docs/handbook/enums.html

---
+++ typescript_enum.ts
@@ -0,0 +1,7 @@
+// https://www.typescriptlang.org/docs/handbook/enums.html
+enum Directions{
+    up,down,
+    left,right
+}
+console.log(Directions.up.valueOf())
+console.log(Directions.down.valueOf())
